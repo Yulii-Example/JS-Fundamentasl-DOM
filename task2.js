9be05c70d90176bfe8012e5f35f5a0d12814eb95
Yulii <yulikkrasulia@gmail.com>
2024-02-26 17:34:46 +0200

    done with task2

--- task2.js
+++ task2.js
@@ -3,8 +3,26 @@
 
 
 function applyStyles() {
-     
+     const titleEl = document.getElementById("title");
+     titleEl.style.backgroundColor = "green";
+     const list = document.getElementById("myDiv");
+     const first = list.children[0];
+     first.style.fontWeight = "bold";
+     const second = list.children[1];
+     second.style.color = "red";
+     const third = list.children[2];
+     third.style.textDecoration = "underline";
+     const fourth = list.children[3];
+     fourth.style.fontStyle = "italic";
+     const myList = document.getElementById("myList");
+     const listItems = myList.getElementsByTagName("li");
+     let combinedLi = "";
+     for (let i = 0; i < listItems.length; i++) {
+        combinedLi += listItems[i].innerText;
+     } 
+     myList.innerText = combinedLi;
 }
+applyStyles()
 
 
 module.exports = applyStyles;
