9e093cb1fd7b2e9c065259fc96b6daf01e32acdf
Yulii <yulikkrasulia@gmail.com>
2024-02-29 16:25:44 +0200

    done with task4

--- task4.js
+++ task4.js
@@ -5,6 +5,21 @@
 
 // Ваш код ..
 
+const form = document.querySelector('.array fieldset');
+
+form.addEventListener('click', function (event) {
+    event.preventDefault();
+
+    const name = form.querySelector('[name="fio"]').value;
+    const phoneNumber = form.querySelector('[name="phone"]').value;
+    const dateOfBirth = form.querySelector('[name="birthday"]').value;
+    const emailAddress = form.querySelector('[name="email"]').value;
+
+    console.log("name:", name);
+    console.log("phoneNumber:", phoneNumber);
+    console.log("dateOfBirth:", dateOfBirth);
+    console.log("emailAddress:", emailAddress);
+});
 
 module.exports = {
     name,
